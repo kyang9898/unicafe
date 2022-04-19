d1850d7e312c6d12e96382c6a57216a814121e08
kyang9898 <kyang0908@gmail.com>
2022-04-18 23:32:49 -0400

    1.6

--- src/App.js
+++ src/App.js
@@ -6,40 +6,43 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <Title name={"give feedback"} />
-      <Button name={"good"} func={setGood} value={good} />
-      <Button name={"neutral"} func={setNeutral} value={neutral} />
-      <Button name={"bad"} func={setBad} value={bad} />
+  //1.6
+//   return (
+//     <div>
+//       <Title name={"give feedback"} />
+//       <Button name={"good"} func={setGood} value={good} />
+//       <Button name={"neutral"} func={setNeutral} value={neutral} />
+//       <Button name={"bad"} func={setBad} value={bad} />
 
-      <Title name={"statistics"} />
-      <Counter name={"good"} number={good} />
-      <Counter name={"neutral"} number={neutral} />
-      <Counter name={"bad"} number={bad} />
-    </div>
-  );
-};
+//       <Title name={"statistics"} />
+//       <Counter name={"good"} number={good} />
+//       <Counter name={"neutral"} number={neutral} />
+//       <Counter name={"bad"} number={bad} />
+//     </div>
+//   );
+// };
 
-const Title = (props) => {
-  return <h2> {props.name} </h2>;
-};
-const Button = (props) => {
-  return (
-    <button onClick={setToValue(props.func, props.value + 1)}>
-      {props.name}
-    </button>
-  );
-};
-const Counter = (props) => {
-  return (
-    <div>
-      {props.name} {props.number}
-    </div>
-  );
-};
-const setToValue = (func, newValue) => () => {
-  func(newValue);
-};
+// const Title = (props) => {
+//   return <h2> {props.name} </h2>;
+// };
+// const Button = (props) => {
+//   return (
+//     <button onClick={setToValue(props.func, props.value + 1)}>
+//       {props.name}
+//     </button>
+//   );
+// };
+// const Counter = (props) => {
+//   return (
+//     <div>
+//       {props.name} {props.number}
+//     </div>
+//   );
+// };
+// const setToValue = (func, newValue) => () => {
+//   func(newValue);
+// };
+
+//1.7
 
 export default App;
