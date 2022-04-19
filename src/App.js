2a34c8fa8dd19262343b69da8a7368319109ba8c
kyang9898 <kyang0908@gmail.com>
2022-04-19 00:52:49 -0400

    1.7

--- src/App.js
+++ src/App.js
@@ -5,44 +5,45 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + bad + neutral;
 
-  //1.6
-//   return (
-//     <div>
-//       <Title name={"give feedback"} />
-//       <Button name={"good"} func={setGood} value={good} />
-//       <Button name={"neutral"} func={setNeutral} value={neutral} />
-//       <Button name={"bad"} func={setBad} value={bad} />
+  return (
+    <div>
+      <Title name={"give feedback"} />
+      <Button name={"good"} func={setGood} value={good} />
+      <Button name={"neutral"} func={setNeutral} value={neutral} />
+      <Button name={"bad"} func={setBad} value={bad} />
 
-//       <Title name={"statistics"} />
-//       <Counter name={"good"} number={good} />
-//       <Counter name={"neutral"} number={neutral} />
-//       <Counter name={"bad"} number={bad} />
-//     </div>
-//   );
-// };
+      <Title name={"statistics"} />
+      <Counter name={"good"} number={good} />
+      <Counter name={"neutral"} number={neutral} />
+      <Counter name={"bad"} number={bad} />
+      <Counter name={"all"} number={all} />
+      <Counter name={"average"} number={(good - bad) / all} />
+      <Counter name={"positive"} number={String((good / all) * 100) + "%"} />
+    </div>
+  );
+};
 
-// const Title = (props) => {
-//   return <h2> {props.name} </h2>;
-// };
-// const Button = (props) => {
-//   return (
-//     <button onClick={setToValue(props.func, props.value + 1)}>
-//       {props.name}
-//     </button>
-//   );
-// };
-// const Counter = (props) => {
-//   return (
-//     <div>
-//       {props.name} {props.number}
-//     </div>
-//   );
-// };
-// const setToValue = (func, newValue) => () => {
-//   func(newValue);
-// };
-
-//1.7
+const Title = (props) => {
+  return <h2> {props.name} </h2>;
+};
+const Button = (props) => {
+  return (
+    <button onClick={setToValue(props.func, props.value + 1)}>
+      {props.name}
+    </button>
+  );
+};
+const Counter = (props) => {
+  return (
+    <div>
+      {props.name} {props.number}
+    </div>
+  );
+};
+const setToValue = (func, newValue) => () => {
+  func(newValue);
+};
 
 export default App;
