25ce6b3a63dbaff0e6cafe117b96d44dd0ea4dfc
kyang9898 <kyang0908@gmail.com>
2022-04-18 23:20:49 -0400

    1.7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title name={"give feedback"} />
+      <Button name={"good"} func={setGood} value={good} />
+      <Button name={"neutral"} func={setNeutral} value={neutral} />
+      <Button name={"bad"} func={setBad} value={bad} />
+
+      <Title name={"statistics"} />
+      <Counter name={"good"} number={good} />
+      <Counter name={"neutral"} number={neutral} />
+      <Counter name={"bad"} number={bad} />
     </div>
   );
-}
+};
+
+const Title = (props) => {
+  return <h2> {props.name} </h2>;
+};
+const Button = (props) => {
+  return (
+    <button onClick={setToValue(props.func, props.value + 1)}>
+      {props.name}
+    </button>
+  );
+};
+const Counter = (props) => {
+  return (
+    <div>
+      {props.name} {props.number}
+    </div>
+  );
+};
+const setToValue = (func, newValue) => () => {
+  func(newValue);
+};
 
 export default App;
